6e332bcb11b94c111316ec6d50072c96786cc77b
Prateek <prateektheone@gmail.com>
2012-08-27 11:38:23 +0530

    #prateek,manali: Creation Of New Event With separate date and time fields

--- src/test/java/com/thoughtworks/twu/domain/EventMapperTest.java
+++ src/test/java/com/thoughtworks/twu/domain/EventMapperTest.java
@@ -17,16 +17,16 @@ public class EventMapperTest extends IntegrationTest {
     private EventMapper eventMapper;
 
     @Test
-    public void shouldInsertEvent() throws Exception {
+    public void shouldVerifyCorrectInsertionOfEvent() throws Exception {
         String venue="Pune Office";
-        String when="23-08-2012 10:05 am";
+        String when="23/08/2012";
         String title="XConf";
         String description="Ruby Conference";
-
+        String time="12:01 pm";
         String owner="Aman King";
         Presentation actualPresentation = new Presentation(title, description, owner);
-        Event event=new Event(actualPresentation,venue,when);
-        Event secondEvent=new Event(actualPresentation,"sjafh","kxcvn");
+        Event event=new Event(actualPresentation,venue,when,time);
+        Event secondEvent=new Event(actualPresentation,"sjafh","kxcvn","sdfjde");
         assertThat(eventMapper.insertEvent(event),is(1));
         assertThat(eventMapper.insertEvent(secondEvent),not(2));
 
@@ -38,18 +38,18 @@ public class EventMapperTest extends IntegrationTest {
     /* @Test
 public void shouldCheckCreationOfEvent() throws Exception {
 String venue="Pune Office";
-String when="23-08-2012 10:05 am";
+String date_="23-08-2012 10:05 am";
 String title="XConf";
 String description="Ruby Conference";
 
 String owner="Aman King";
 
 
-Event xConfPune=new Event(title,description,venue,when);
+Event xConfPune=new Event(title,description,venue,date_);
 
 presentationMapper.insertPresentation(new Presentation(title,description,owner));
 int presentationId=presentationMapper.getPresentationByTitle(title).getId();
-eventMapper.insertEvent(presentationId,venue,when);
+eventMapper.insertEvent(presentationId,venue,date_);
 
 Event expectedEvent=eventMapper.getEventByName("XConf Pune");
 assertThat(expectedEvent,is(xConfPune));
