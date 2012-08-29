bb2fc8f69550e2de015b8c9e242f7f5caefb88dc
chenqingspring <chenqing1213@gmail.com>
2012-08-29 18:47:39 +0800

    #10 - Ilias, Chen, Manali - refactoring moved ApplicationClock in Service layer

--- src/test/java/com/thoughtworks/twu/controller/FeedbackControllerTest.java
+++ src/test/java/com/thoughtworks/twu/controller/FeedbackControllerTest.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.twu.controller;
 
-import com.thoughtworks.twu.domain.Feedback;
 import com.thoughtworks.twu.service.FeedbackService;
-import com.thoughtworks.twu.utils.TestClock;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,16 +16,14 @@ public class FeedbackControllerTest {
     public void shouldEnterAFeedback() {
         // Given
         FeedbackService feedbackService = mock(FeedbackService.class);
-        TestClock testClock = new TestClock();
-        feedbackController = new FeedbackController(feedbackService, testClock);
+        feedbackController = new FeedbackController(feedbackService);
         int talkId = 9;
-        Feedback feedback = new Feedback(talkId, "Feedback comment", "feedback giver name", "caroline@example.com", testClock.now());
         // When
         ModelAndView result = feedbackController.enterFeedback(talkId,"Feedback comment");
         // Then
         String resultMessage = (String) result.getModel().get("result-message");
         assertThat(resultMessage, is("Thank you for the feedback"));
-        verify(feedbackService).enterFeedback(feedback);
+        verify(feedbackService).enterFeedback(talkId, "Feedback comment", "feedback giver name", "caroline@example.com");
     }
 
 }
--- src/test/java/com/thoughtworks/twu/service/FeedbackServiceTest.java
+++ src/test/java/com/thoughtworks/twu/service/FeedbackServiceTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.twu.service;
 
 import com.thoughtworks.twu.domain.Feedback;
 import com.thoughtworks.twu.persistence.FeedbackMapper;
+import com.thoughtworks.twu.utils.TestClock;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
@@ -12,11 +13,12 @@ public class FeedbackServiceTest {
     public void shouldInsertFeedback() {
         //Given
         FeedbackMapper mockFeedback = mock(FeedbackMapper.class);
-        FeedbackService feedbackService = new FeedbackService(mockFeedback);
-        Feedback feedback = new Feedback();
+        TestClock testClock = new TestClock();
+        FeedbackService feedbackService = new FeedbackService(mockFeedback, testClock);
+        Feedback feedback = new Feedback(9, "Feedback comment", "feedback giver name", "caroline@example.com", testClock.now());
 
         //When
-        feedbackService.enterFeedback(feedback);
+        feedbackService.enterFeedback(9, "Feedback comment", "feedback giver name", "caroline@example.com");
 
         //Then
         verify(mockFeedback).insertFeedback(feedback);
