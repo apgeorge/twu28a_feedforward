fbdbac91818d2bae37158a6691f9d9485c9cf813
Teddy <tku@u.northwestern.edu>
2012-08-29 11:53:11 +0530

    teddy,prateek: retrieve feedback list by talk ID method added

--- src/main/java/com/thoughtworks/twu/persistence/FeedbackMapper.java
+++ src/main/java/com/thoughtworks/twu/persistence/FeedbackMapper.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 public interface FeedbackMapper {
     @Insert("INSERT INTO feedback (feedback_comment,talk_id,attendee,attendee_mail) VALUES(#{feedbackComment},#{talkId},#{attendee},#{attendeeMail})")
     int insertFeedback(Feedback feedback);
@@ -18,4 +20,14 @@ public interface FeedbackMapper {
             @Result(property = "attendeeMail", column = "attendee_mail")
     })
     Feedback getLastEnteredFeedback();
+
+    @Select("SELECT talk_id,feedback_comment,attendee,attendee_mail FROM feedback WHERE talk_id = #{talk_id} ORDER BY time_stamp DESC")
+    @Results(value = {
+            @Result(property = "feedbackComment", column = "feedback_comment"),
+            @Result(property = "talkId", column = "talk_id"),
+            @Result(property = "attendee", column = "attendee"),
+            @Result(property = "attendeeMail", column = "attendee_mail")
+    })
+    List<Feedback> getFeedbackByTalkId(int talk_id);
+
 }
--- src/test/java/com/thoughtworks/twu/persistence/FeedbackMapperTest.java
+++ src/test/java/com/thoughtworks/twu/persistence/FeedbackMapperTest.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.twu.persistence;
 
 import com.thoughtworks.twu.domain.Feedback;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -27,21 +29,24 @@ public class FeedbackMapperTest extends IntegrationTest {
     }
 
     @Test
-    public void shouldRetrieveFeedbackListByTalkTitle() throws Exception {
-
+    public void shouldRetrieveFeedbackListByTalkId() throws Exception {
+        //Given
         Feedback feedback1 = new Feedback(1, "feedbackComment1", "attendee1", "attendeeMail 1");
-        feedbackMapper.insertFeedback(feedback1);
-        Feedback feedback2 = new Feedback(1, "feedbackComment2", "attendee2", "attendeeMail 2");
-        feedbackMapper.insertFeedback(feedback2);
         Feedback feedback3 = new Feedback(1, "feedbackComment3", "attendee3", "attendeeMail 3");
-        feedbackMapper.insertFeedback(feedback3);
         Feedback feedback = new Feedback(2, "feedbackComment1", "attendee1", "attendeeMail 1");
+        Feedback feedback2 = new Feedback(1, "feedbackComment2", "attendee2", "attendeeMail 2");
+        feedbackMapper.insertFeedback(feedback1);
+        feedbackMapper.insertFeedback(feedback2);
+        feedbackMapper.insertFeedback(feedback3);
         feedbackMapper.insertFeedback(feedback);
         ArrayList<Feedback> feedbackList = new ArrayList<Feedback>();
-        feedbackList.add(feedback1);
-        feedbackList.add(feedback2);
         feedbackList.add(feedback3);
-        assertThat(feedbackList.toString(),feedbackMapper.getFeedbackByTalkId(1));
+        feedbackList.add(feedback2);
+        feedbackList.add(feedback1);
+        //When
+        List<Feedback> result = feedbackMapper.getFeedbackByTalkId(1);
+        //Then
+        Assert.assertEquals(feedbackList, result);
 
     }
 }
