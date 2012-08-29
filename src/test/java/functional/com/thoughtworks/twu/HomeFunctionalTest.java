7f0b095a17315d67aec73db26f82a7c473f785db
Prateek <prateektheone@gmail.com>
2012-08-29 14:14:54 +0530

    Fixed Firefox Driver

--- src/test/java/functional/com/thoughtworks/twu/HomeFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/HomeFunctionalTest.java
@@ -18,7 +18,7 @@ public class HomeFunctionalTest {
 
     @Before
     public void setUp() {
-        webDriver = new InternetExplorerDriver();
+        webDriver = new FirefoxDriver();
     }
 
     @Test
