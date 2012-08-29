08afa43a28182c7bd8adb025f7acb41aa0017d90
arunscaria91 <arunscaria91@gmail.com>
2012-08-29 09:35:01 +0530

    #7 Scari & Caroline - Talk mapper test that reflects the auto increment to object used

--- src/main/java/com/thoughtworks/twu/domain/Presentation.java
+++ src/main/java/com/thoughtworks/twu/domain/Presentation.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.twu.domain;
 
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public class Presentation {
     private String title;
     private String description;
@@ -15,33 +19,30 @@ public class Presentation {
         this.owner = owner;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Presentation that = (Presentation) o;
-
-        if (description != null ? !description.equals(that.description) : that.description != null) return false;
-        if (owner != null ? !owner.equals(that.owner) : that.owner != null) return false;
-        if (title != null ? !title.equals(that.title) : that.title != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = title != null ? title.hashCode() : 0;
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (owner != null ? owner.hashCode() : 0);
-        return result;
-    }
-
     public int getId() {
         return id;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
