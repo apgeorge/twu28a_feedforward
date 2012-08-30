8ef70fafc90cc141a832d4ede1b7730412a1d638
Caroline <Caroline@TW>
2012-08-30 14:34:01 +0530

    ML + CK: created No Event Message Test

--- src/test/java/functional/com/thoughtworks/twu/ViewEventDetailsFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/ViewEventDetailsFunctionalTest.java
@@ -1,5 +1,6 @@
 package functional.com.thoughtworks.twu;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -15,29 +16,36 @@ public class ViewEventDetailsFunctionalTest {
 
     public static final int HTTP_PORT = 9191;
     public static final String HTTP_BASE_URL = "http://localhost:" + HTTP_PORT + "/twu/home.html";
+
     private FirefoxDriver driver;
+    private WebDriverWait wait;
+
+    @After
+    public void tearDown() throws Exception {
+        driver.close();
+    }
 
     @Before
     public void setUp() {
 
         driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 20);
     }
 
     @Test
     public void shouldDisplayMessageIfNoEventIsPresent() {
         //Given
-        String s = "http://10.10.15.134:8080/bacon/home.html";
         driver.get(HTTP_BASE_URL);
         //When
         WebElement myTalksButton = driver.findElement(By.id("my_talks_button"));
         myTalksButton.click();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("1")));
         WebElement talksButton = driver.findElement(By.id("1"));
         talksButton.click();
-        WebElement text = driver.findElement(By.className("ui-btn-text"));
+        WebElement text = driver.findElement(By.id("noeventmessage"));
         //Then
-        assertThat(text.getText(), is("There are no talks present at this moment"));
+        assertThat(text.getText(), is("There are no talks at this moment."));
     }
 
 
