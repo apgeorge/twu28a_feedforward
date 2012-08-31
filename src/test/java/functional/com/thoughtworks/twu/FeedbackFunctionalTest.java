0a7e6f7848074af41aa82f632df71d67b26bcb44
chenqingspring <chenqing1213@gmail.com>
2012-08-31 13:19:13 +0800

    #10: Manali, Harsha, Ilias Enter Feedback Functional Test Added

--- src/test/java/functional/com/thoughtworks/twu/FeedbackFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/FeedbackFunctionalTest.java
@@ -4,6 +4,7 @@ import com.thoughtworks.twu.domain.Presentation;
 import com.thoughtworks.twu.persistence.PresentationMapper;
 import com.thoughtworks.twu.persistence.TalkMapper;
 import com.thoughtworks.twu.service.TalkService;
+import com.thoughtworks.twu.utils.WaitForAjax;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +13,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class FeedbackFunctionalTest {
-    public static final int HTTP_PORT = 9091;
+    public static final int HTTP_PORT = 9191;
     public static final String HTTP_BASE_URL = "http://localhost:" + HTTP_PORT + "/twu/home.html";
     private WebDriver webDriver;
     private TalkMapper mockTalkMapper;
@@ -32,6 +38,28 @@ public class FeedbackFunctionalTest {
         talkService.createTalkWithNewPresentation(presentation, "venue", "date", "time");
         webDriver.get(HTTP_BASE_URL);
 
+    }
+
+    @Test
+    public void shouldBeAbleToEnterFeedbackOnTalk() throws InterruptedException {
+        WebElement talkLink = webDriver.findElement(By.linkText("Test Talk"));
+        talkLink.click();
+        WaitForAjax.WaitForAjax(webDriver);
+        assertTrue(webDriver.getPageSource().contains("Past Feedback"));
+        int countInitial= countNoOfFeedbacks();
+        WebElement feedbackTextBox = webDriver.findElement(By.id("feedback_text"));
+        feedbackTextBox.sendKeys("New Feedback");
+        WebElement feedbackSubmitButton= webDriver.findElement(By.id("add_feedback_submit"));
+        feedbackSubmitButton.click();
+        WaitForAjax.WaitForAjax(webDriver);
+        int countNewFeedbacks=countNoOfFeedbacks()-countInitial;
+        assertThat(countNewFeedbacks, is(1));
+    }
+
+    private int countNoOfFeedbacks()
+    {
+        List<WebElement> feedbackList= webDriver.findElements(By.className("feedback-item"));
+        return feedbackList.size();
     }
 
     @After
