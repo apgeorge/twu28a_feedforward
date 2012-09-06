6f84f2682f607f23469e5cc447685cddb7c4e326
chenqingspring <chenqing1213@gmail.com>
2012-09-06 13:21:01 +0800

    chen & scari - build fix :(

--- src/test/java/functional/com/thoughtworks/twu/TalksHomePage.java
+++ src/test/java/functional/com/thoughtworks/twu/TalksHomePage.java
@@ -46,16 +46,15 @@ public class TalksHomePage {
 
     @Test
     public void shouldBeAbleToCreateNewTalk() throws InterruptedException {
-        WebElement myTalksButton = webDriver.findElement(By.id("my_talks_button"));
-        myTalksButton.click();
-        WaitForAjax(webDriver);
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        javascriptExecutor.executeScript("$('#my_talks_button').click();");
         assertTrue(webDriver.findElement(By.id("new_talk")).isDisplayed());
         webDriver.findElement(By.id("new_talk")).click();
         assertTrue(webDriver.findElement(By.id("title")).isDisplayed());
         webDriver.findElement(By.id("title")).sendKeys(now().toString());
         //webDriver.findElement(By.id("description")).sendKeys("Seven wise men");
         webDriver.findElement(By.id("venue")).sendKeys("Ajanta Ellora");
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+
         javascriptExecutor.executeScript("$('#datepicker').val('28/09/2012')");
         javascriptExecutor.executeScript("$('#timepicker').val('11:42 AM')");
         javascriptExecutor.executeScript("$('#new_talk_submit').click()");
