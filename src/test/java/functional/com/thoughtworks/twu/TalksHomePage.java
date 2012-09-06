a23db1d832892b44e8886679f2189ba165c88503
chenqingspring <chenqing1213@gmail.com>
2012-09-06 13:27:55 +0800

    chen & scari - build fixed :)

--- src/test/java/functional/com/thoughtworks/twu/TalksHomePage.java
+++ src/test/java/functional/com/thoughtworks/twu/TalksHomePage.java
@@ -45,14 +45,14 @@ public class TalksHomePage {
     }
 
     @Test
-    public void shouldBeAbleToCreateNewTalk() throws InterruptedException {
+    public void shouldBeAbleToCreateNewTalkWithDescription() throws InterruptedException {
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
         javascriptExecutor.executeScript("$('#my_talks_button').click();");
         assertTrue(webDriver.findElement(By.id("new_talk")).isDisplayed());
         webDriver.findElement(By.id("new_talk")).click();
         assertTrue(webDriver.findElement(By.id("title")).isDisplayed());
         webDriver.findElement(By.id("title")).sendKeys(now().toString());
-        //webDriver.findElement(By.id("description")).sendKeys("Seven wise men");
+        webDriver.findElement(By.id("description")).sendKeys("Seven wise men");
         webDriver.findElement(By.id("venue")).sendKeys("Ajanta Ellora");
 
         javascriptExecutor.executeScript("$('#datepicker').val('28/09/2012')");
