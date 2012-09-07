aca01c7e4b38026ba0bc54ed7e184232be7a660f
Prateek <prateektheone@gmail.com>
2012-09-07 13:34:01 +0530

    Harsha, Lokesh : Recent Talks Functional Test

--- src/test/java/functional/com/thoughtworks/twu/ViewListOfUpcomingTalksTest.java
+++ src/test/java/functional/com/thoughtworks/twu/ViewListOfUpcomingTalksTest.java
@@ -2,9 +2,11 @@ package functional.com.thoughtworks.twu;
 
 import com.thoughtworks.twu.persistence.PresentationMapper;
 import com.thoughtworks.twu.persistence.TalkMapper;
+import com.thoughtworks.twu.utils.DateParser;
 import functional.com.thoughtworks.twu.utils.Cas;
 import functional.com.thoughtworks.twu.utils.Talk;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -41,17 +44,17 @@ public class ViewListOfUpcomingTalksTest  {
     }
 
     @Test
-    (expected=NoSuchElementException.class)
+    (expected = NoSuchElementException.class)
     public void shouldNotDisplayFeedbackTextboxForUpcomingTalks(){
-        String date= DateTime.now().plusDays(3).toDate().toString();
-        Talk talk=new Talk("RubyConf","Learn Ruby", "Ajanta-Ellora",date,"11:42 AM");
+        DateTime dateTime=DateTime.now().plusDays(3);
+        Talk talk=new Talk("RubyConf","Learn Ruby", "Ajanta-Ellora",dateTime.toString("dd/MM/YYYY"),"11:42 AM");
         talk.newTalk(webDriver);
         WebElement upcomingTalksButton =webDriver.findElement(By.id("upcoming_talks_button"));
         upcomingTalksButton.click();
         List<WebElement> listOfTalks= webDriver.findElements(By.className("ui-link-inherit"));
         listOfTalks.get(0).click();
         webDriver.findElement(By.id("feedback_text"));
-    }
+     }
 
 
     @After
